7528d6401ccf998e3965714e9edfb8ec8b09554d
Christopher Girvin <chrisg8691@gmail.com>
2021-02-17 11:49:13 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,133 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.less';
+import { Avatar, Button, Card, Col, Row } from 'antd';
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from '@ant-design/icons';
+import styled from 'styled-components';
+
+const fakeData = [
+  {
+    programid: Math.random().toString(),
+    program_name: 'Intro to Computer Science',
+    program_type: 'education_higher',
+    program_description: 'this is a test cs description',
+  },
+  {
+    programid: Math.random().toString(),
+    program_name: 'Intro to Python',
+    program_type: 'education_higher',
+    program_description: 'this is a test for python decscription',
+  },
+  {
+    programid: Math.random().toString(),
+    program_name: 'Pre-Calculus',
+    program_type: 'education_k12',
+    program_description: 'this is a test for pre-calc',
+  },
+  {
+    programid: Math.random().toString(),
+    program_name: 'Information Technology Certification',
+    program_type: 'training',
+    program_description: 'this is a test for IT certification',
+  },
+  {
+    programid: Math.random().toString(),
+    program_name: 'Microsoft Excel - From Beginner to Expert',
+    program_type: 'training',
+    program_description: 'this is a test for excel training',
+  },
+  {
+    programid: Math.random().toString(),
+    program_name: 'The Complete Digital Marketing Course',
+    program_type: 'other',
+    program_description: 'this is a test for digital marketing',
+  },
+];
+
+const typeImgArr = {
+  education_k12:
+    'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.UAxmKUj3hhGcCKYZ_XYW2QHaE3%26pid%3DApi&f=1',
+  education_higher:
+    'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.a39ARZ-_NkHD0L56BxYkowAAAA%26pid%3DApi&f=1',
+  training:
+    'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.gBd8xGd6qtcrWZgtV-BAWQHaE7%26pid%3DApi&f=1',
+  other:
+    'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.gBd8xGd6qtcrWZgtV-BAWQHaE7%26pid%3DApi&f=1',
+};
+
+// const StyledCard = styled(Card)`
+//
+// `;
 
 function App() {
+  const { Meta } = Card;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      className="App"
+      style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        textAlign: 'center',
+      }}
+      key="app-container"
+    >
+      {fakeData.map((program) => (
+        <Card
+          key={program.program_id}
+          style={{ width: 300, margin: '10px' }}
+          cover={
+            <img
+              key={program.programid + 'img'}
+              alt="example"
+              src={typeImgArr[program.program_type]}
+            />
+          }
+          actions={[
+            <SettingOutlined key={program.programid + 'setting'} />,
+            <EditOutlined key={program.programid + 'edit'} />,
+            <EllipsisOutlined key={program.programid + 'ellipsis'} />,
+          ]}
         >
-          Learn React
-        </a>
-      </header>
+          <Meta
+            key={program.programid + 'meta'}
+            // avatar={<Avatar src='https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png' />}
+            title={program.program_name}
+            description={program.program_description}
+          />
+        </Card>
+      ))}
+      {/*<Row gutter={16} style={{ margin: '20px' }}>*/}
+      {/*  <Col gutter={8}>*/}
+      {/*    {fakeData.slice(0, Math.floor(fakeData.length / 2)).map((prog) => (*/}
+      {/*      <Card*/}
+      {/*        hoverable*/}
+      {/*        key={prog.programid}*/}
+      {/*        style={{ margin: '20px' }}*/}
+      {/*      >*/}
+      {/*        <h4>{prog.program_name}</h4>*/}
+      {/*        <h5>{prog.program_description}</h5>*/}
+      {/*      </Card>*/}
+      {/*    ))}*/}
+      {/*  </Col>*/}
+      {/*</Row>*/}
+      {/*<Row gutter={16} style={{ margin: '20px' }}>*/}
+      {/*  <Col gutter={8}>*/}
+      {/*    {fakeData.slice(Math.floor(fakeData.length / 2), fakeData.length).map((prog) => (*/}
+      {/*      <Card*/}
+      {/*        hoverable*/}
+      {/*        key={prog.programid}*/}
+      {/*        style={{ margin: '20px' }}*/}
+      {/*      >*/}
+      {/*        <h4>{prog.program_name}</h4>*/}
+      {/*        <h5>{prog.program_description}</h5>*/}
+      {/*      </Card>*/}
+      {/*    ))}*/}
+      {/*  </Col>*/}
+      {/*</Row>*/}
     </div>
   );
 }
